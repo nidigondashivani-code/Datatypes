fa280db1edd661e5cc658c4e189626b08edce893
Shivani Nidigonda <nidigondashivani@gmail.com>
2026-04-04 11:51:19 +0530

    Create findsmall.py

---
+++ findsmall.py
@@ -0,0 +1,7 @@
+firstnumber = int(input("Enter first number:"));
+secondnumber = int(input("Enter Second number:"));
+
+if(firstnumber < secondnumber):
+  print(f"{firstnumber} is smallest than second number")
+else:
+  print(f"{secondnumber} is smallest than firstnumber")
