ce6e33b33279c1744c12db444278dadfc71eb0bc
Shivani Nidigonda <nidigondashivani@gmail.com>
2026-04-04 11:36:27 +0530

    Create findBig.py

---
+++ findBig.py
@@ -0,0 +1,7 @@
+firstnumber = int(input("Enter first number:"));
+secondnumber = int(input("Enter Second number:"));
+
+if(firstnumber > secondnumber):
+  print(f"{firstnumber} is greatest than second number")
+else:
+  print(f"{secondnumber} is greatest than firstnumber")
